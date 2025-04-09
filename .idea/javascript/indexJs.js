9cb734caae0fe342c4d4f8669d8c94072a82c4b1
Jhockinn <Tobiln@hotmail.dk>
2025-04-10 00:09:56 +0200

    tested with hover button

--- .idea/javascript/indexJs.js
+++ .idea/javascript/indexJs.js
@@ -1,1 +1,34 @@
 const yes = yes;
+
+document.addEventListener("DOMContentLoaded",() =>{
+    fetch("/api/recomendations")
+        .then(res => res.json())
+        .then(data => displayGames(data))
+        .catch(err => console.error("Error fetching games:", err));
+});
+
+function displayGames(games){
+
+    const tableCells = document.querySelectorAll("td.game-cell")
+
+    games.forEach((game, index) => {
+        if (index >= tableCells.length) return;
+
+        const td = tableCells[index]
+        const dealsHTML = Object.entries(game.dealList || {})
+            .map(([store,price])=> `<p>${store}: $${price}</p>\\`)
+            .join("")
+
+        td.innerHTML = `
+            <div class ="game-wrapper">
+            <img src=${game.picture}" alt=${game.name}">
+            <div class="hover-info">
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <p><strong>Genres:</strong> ${game.genre.join(", ")}</p>
+            <p><strong>Platforms:</strong> ${game.platform.join(", ")}</p>
+            ${dealsHTML}
+        </div>
+        </div>`;
+    });
+}
