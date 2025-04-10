95c0bb6eb539bd8fc2fa72ad3f81ed0d132b1948
Telity <Nigu0001@Stud.kea.dk>
2025-04-10 12:11:43 +0200

    pictures works

--- .idea/javascript/indexJs.js
+++ .idea/javascript/indexJs.js
@@ -1,20 +1,87 @@
-document.addEventListener("DOMContentLoaded",() =>{
+const formbody = document.getElementById("gameSearch");
+const singlebody = document.getElementById("singleGame");
+
+
+formbody.addEventListener('submit', async function(event) {
+    event.preventDefault()
+
+
+    const formdata = new FormData(formbody);
+    const newdata = Object.fromEntries(formdata)
+    const url = this.action;
+
+
+    try {
+        const response = await fetch(url, {
+            method: this.method,
+            body: JSON.stringify(newdata)
+        })
+
+        if(response.ok){
+            const result = response.json();
+            console.log("Succes")
+            await displayGames(result)
+        }
+        else{
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+    }
+    catch (error) {
+        console.error("Error", error)
+    }
+
+})
+
+singlebody.addEventListener('submit', async function(event) {
+    event.preventDefault()
+
+
+    const formdata = new FormData(formbody);
+    const newdata = Object.fromEntries(formdata)
+    const url = this.action;
+
+
+    try {
+        const response = await fetch(url, {
+            method: this.method,
+            body: JSON.stringify(newdata)
+        })
+
+        if(response.ok){
+            const result = response.json();
+            console.log("Succes")
+            await displayGames(result)
+        }
+        else{
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+    }
+    catch (error) {
+        console.error("Error", error)
+    }
+
+})
+
+
+/*document.addEventListener("DOMContentLoaded",() =>{
     fetch("http://localhost:8080/mistralChoice")
         .then(res => res.json())
         .then(data => displayGames(data))
         .catch(err => console.error("Error fetching games:", err));
-});
+});*/
 
-function displayGames(games){
-
+async function displayGames(gameinfo){
+    console.log(gameinfo)
     const tableCells = document.querySelectorAll("td.game-cell")
+
+    let games = await gameinfo;
 
     games.forEach((game, index) => {
         if (index >= tableCells.length) return;
 
         const td = tableCells[index]
         const dealsHTML = Object.entries(game.dealList || {})
-            .map(([store,price])=> `<p>${store}: $${price}</p>\\`)
+            .map(([store,price])=> `<p>${store}: ${price}</p>`)
             .join("")
 
         td.innerHTML = `
@@ -22,10 +89,10 @@ function displayGames(games){
             <img src="${game.picture}" alt="${game.name}">
             <div class="hover-info">
             <h3>${game.name}</h3>
-            <p>${game.description}</p>
             <p><strong>Genres:</strong> ${game.genre.join(", ")}</p>
             <p><strong>Platforms:</strong> ${game.platform.join(", ")}</p>
             ${dealsHTML}
+            <p>${game.description}</p>
         </div>
         </div>`;
     });
